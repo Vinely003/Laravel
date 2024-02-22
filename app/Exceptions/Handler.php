897787dc7f4a56fa9dc265c6806a74f2d237ef73
Vinely003 <korosi.vince20@gmail.com>
2024-02-22 15:03:55 +0100

    Fix:Handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -22,11 +22,18 @@ class Handler extends ExceptionHandler
     /**
      * Register the exception handling callbacks for the application.
      */
-
+    public function register(): void
+    {
+        $this->reportable(function (Throwable $e) {
+            //
+        });
+    }
     public function render($request, Throwable $exception)
     {
         if ($exception instanceof QueryException) {
             return response()->json(['error' => 'Unable to connect to the database.'], 500);
         }
+
+        return parent::render($request, $exception);
     }
 }
